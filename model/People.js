8eba9f832d5342be6eec753495e6e93ff2197354
Rashidul Islam <66661887+mrirasel@users.noreply.github.com>
2025-11-06 18:43:19 +0600

    update model, validator midleware

---
+++ model/People.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const peopleSchema = mangoose.Schema(
+    {
+        name:{
+            type:String,
+            required: true,
+            trim: true
+        },
+        email:{
+            type:String,
+            required: true,
+            trim: true, 
+            lowercase: true,
+        },
+        mobile:{
+            type:String,
+            required: true,
+        },
+        password:{
+            type: String
+        },
+         avatar: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    },
+    {
+    timestamps: true,
+  }
+);
+
+const People = mongoose.model("People", peopleSchema);
+
+module.exports = People;
