9d1da7c0968df35a91821ecae378224bf2bdc605
Rashidul Islam <66661887+mrirasel@users.noreply.github.com>
2025-11-12 20:30:48 +0600

    login, logout mechanism added

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -5,8 +5,9 @@ const router = express.Router();
 //internal import 
 const {getInbox} = require('../controller/inboxController');
 const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
+const { checkLogin } = require("../middleware/common/checkLogin");
 
 
 //Users page
-router.get("/",decorateHtmlResponse("Inbox"), getInbox);
+router.get("/",decorateHtmlResponse("Inbox"),checkLogin, getInbox);
 module.exports = router;
--- router/loginRoute.js
+++ router/loginRoute.js
@@ -3,10 +3,31 @@ const express = require("express");
 const router = express.Router();
 
 //internal import 
-const {getLogin} = require('../controller/loginController');
+const { getLogin, login, logout } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
 
+const {
+  doLoginValidators,
+  doLoginValidationHandler,
+} = require("../middleware/login/loginValidator");
+
+const { redirectLoggedIn } = require("../middleware/common/checkLogin");
+//set page title 
+const page_title= "Login";
 
 //login page
-router.get("/",decorateHtmlResponse("Login"), getLogin);
+router.get("/",decorateHtmlResponse(page_title),redirectLoggedIn, getLogin);
+
+// process login
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
+
+// logout
+router.delete("/", logout);
+
 module.exports = router;
